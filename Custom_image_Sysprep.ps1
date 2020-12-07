7428277b05f965a96ac94112ac783c79a3b5cfbc
Murugan-MK <73891931+Murugan-MK@users.noreply.github.com>
2020-12-08 02:27:48 +0530

    Script to create custom image from a golden image
    This script will help to create custom image using a golden VM/Image

---
+++ Custom_image_Sysprep.ps1
@@ -0,0 +1,17 @@
+﻿# PowerShell commands to create custom image
+
+# Stopping VM forcibliy before Generalizing
+Stop-AzureRmVM -ResourceGroupName "RGImage" -Name "GoldenVM" -Force
+
+# Generalize the VM
+Set-AzureRmVM -ResourceGroupName "RGImage" -Name "GoldenVM" -Generalized
+
+# Setting VMName
+$GoldenVM=Get-AzureRmVM -ResourceGroupName "RGImage" -Name "GoldenVM"
+
+# Setting VM ID
+ $image=New-AzImageConfig -Location "East US" -SourceVirtualMachineId $GoldenVM.id
+
+ # Creating a new Azure Image
+ New-AzImage -Image $image -ImageName "MK_W2019_Datacenter" -ResourceGroupName "RGImage"
+
